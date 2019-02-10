289894029fddda5c88b50339210feb0ec1df6db4
orr1nuycruz <orr1n@hotmail.com>
2019-02-09 21:58:32 -0500

    reorganize folder structure

--- server/config/app.js
+++ server/config/app.js
@@ -6,7 +6,7 @@ let logger = require('morgan');
 
 //database setup
 let mongoose = require('mongoose');
-let DB = require('./config/db');
+let DB = require('./db');
 
 //point Mongoose to DB URI
 mongoose.connect(DB.URI);
@@ -17,26 +17,28 @@ mongoDB.once('open', ()=>{
 console.log("Connected to MongoDB...");
 });
 
-let indexRouter = require('./routes/index');
-let contactRouter = require('./routes/contact');
+let indexRouter = require('../routes/index');
+let contactRouter = require('../routes/contact');
 // let aboutRouter = require('./routes/about');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
 // app.use('/about-list', aboutRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
