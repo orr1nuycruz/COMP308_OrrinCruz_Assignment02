1cd0342a21e1485e0d837dcfdd8e23a8b895bed4
orr1nuycruz <orr1n@hotmail.com>
2019-03-30 00:16:02 -0400

    Login, Logout, and Register is working properly

--- server/config/app.js
+++ server/config/app.js
@@ -1,8 +1,17 @@
+// modules for node and express
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+
+//configure for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 
 //database setup
 let mongoose = require('mongoose');
@@ -32,6 +41,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+// setup express-session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+//initialize flash
+app.use(flash());
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport user configuration
+
+// create a User Model
+let userModel = require('../models/user');
+let User = userModel.User;
+
+// implement a User authentication
+passport.use(User.createStrategy());
+
+// serialize and deserialize
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 let indexController = require('../controllers/index');
 
-/* GET home page. */
+/* GET pages from the Nav menu. */
 router.get('/',  indexController.displayHomePage);
 
 router.get('/about', indexController.displayAboutPage);
@@ -13,6 +13,23 @@ router.get('/projects', indexController.displayProductsPage);
 router.get('/services', indexController.displayServicesPage);
 
 router.get('/contact', indexController.displayContactPage);
+
+
+
+/* GET - displays the login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST - processes the login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET - displays the User Registration page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST - processes the User Registration page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET - perform user logout */
+router.get('/logout', indexController.performLogout);
 
 
 module.exports = router;
